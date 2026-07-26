999b7627368f52fe20713c29d26f1852a1f5beea
Parker Peterson <peterson.parker.james@gmail.com>
2026-07-26 16:17:56 -0500

    Audio is selected from the nearest recorded tempo

--- scripts/practice_manager.gd
+++ scripts/practice_manager.gd
@@ -8,14 +8,24 @@ extends Node3D
 @onready var control_pattern: ControlPattern = $ControlPattern
 
 
+var audio_path_prefix: StringName = "res://assets/audio/voices/parker/"
+var audio_tempo: String = "120/"
+
+
 func _ready() -> void:
 	Metronome.turn_off()
-	command_audio_manager.set_pitch_scale(Metronome.bpm / 120.0)
-	await get_tree().create_timer(2.0).timeout
-	Metronome.turn_on()
 	
-	#var command_queue = control_pattern.create_queue()
-	#print(command_queue)
+	var tempo_dirs := Array(DirAccess.get_directories_at(audio_path_prefix))
+	var recorded_tempos := tempo_dirs.map(func(dir): return int(dir))
+	recorded_tempos.sort_custom(
+		func(a: int, b: int): return abs(a - Metronome.bpm) < abs(b - Metronome.bpm)
+	)
+	audio_tempo = str(recorded_tempos[0]) + "/"
+	
+	command_audio_manager.set_pitch_scale(float(Metronome.bpm) / float(recorded_tempos[0]))
+	
+	await get_tree().create_timer(1.0).timeout
+	Metronome.turn_on()
 
 
 func _on_command_selected(command: int) -> void:
@@ -24,7 +34,7 @@ func _on_command_selected(command: int) -> void:
 	if command == Commands.NOTHING:
 		return
 	
-	var line_path: StringName = "res://assets/audio/voices/parker/120"+command_string+".mp3"
+	var line_path: StringName = audio_path_prefix + audio_tempo + command_string + ".mp3"
 	command_audio_manager.play(line_path)
 	
 	await Metronome.beat
